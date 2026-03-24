d8171ee0cad99455a098757dffc7079f9bb19e00
Ali Kozhabay <a.kozhabay@globerce.capital>
2026-03-24 18:49:33 +0500

    first commit

---
+++ src/main/java/com/learning/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.learning.controller;
+
+import com.learning.entity.User;
+import com.learning.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+}
---
+++ src/main/java/com/learning/entity/User.java
@@ -0,0 +1,30 @@
+package com.learning.entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
